9da1592f5b0f58537adcd92cc75957471f7b2dd5
Eddy Almuiña <almuina.e@gmail.com>
2024-07-25 13:57:54 +0200

    Defining Variables

--- src/utils.rs
+++ src/utils.rs
@@ -14,6 +14,20 @@ pub(crate) fn extract_digits(s: &str) -> (&str, &str) {
     take_while(|c| c.is_ascii_digit(), s)
 }
 
+pub(crate) fn extract_ident(s: &str) -> (&str, &str) {
+    let input_starts_with_alphabetic = s
+        .chars()
+        .next()
+        .map(|c| c.is_ascii_alphabetic())
+        .unwrap_or(false);
+
+    if input_starts_with_alphabetic {
+        take_while(|c| c.is_ascii_alphanumeric(), s)
+    } else {
+        (s, "")
+    }
+}
+
 pub(crate) fn extract_whitespace(s: &str) -> (&str, &str) {
     take_while(|c| c == ' ', s)
 }
@@ -25,6 +39,14 @@ pub(crate) fn extract_op(s: &str) -> (&str, &str) {
     }
 
     (&s[1..], &s[0..1])
+}
+
+pub(crate) fn tag<'a, 'b>(starting_text: &'a str, s: &'b str) -> &'b str {
+    if s.starts_with(starting_text) {
+        &s[starting_text.len()..]
+    } else {
+        panic!("expected {}", starting_text);
+    }
 }
 
 #[cfg(test)]
@@ -64,5 +86,25 @@ mod tests {
     #[test]
     fn extract_slash() {
         assert_eq!(extract_op("/4"), ("4", "/"));
+    }
+
+    #[test]
+    fn extract_alphabetic_ident() {
+        assert_eq!(extract_ident("abcdEFG stop"), (" stop", "abcdEFG"));
+    }
+
+    #[test]
+    fn extract_alphanumeric_ident() {
+        assert_eq!(extract_ident("foobar1()"), ("()", "foobar1"));
+    }
+
+    #[test]
+    fn cannot_extract_ident_beginning_with_number() {
+        assert_eq!(extract_ident("123abc"), ("123abc", ""));
+    }
+
+    #[test]
+    fn tag_word() {
+        assert_eq!(tag("let", "let a"), " a");
     }
 }
